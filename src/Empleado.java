e545bc56a74809165fb76465e5feeb8097610478
Camilyyyy <camilystefaniasoza@gmail.com>
2025-11-06 12:53:51 -0500

    metodo mostrarInfo

--- src/Empleado.java
+++ src/Empleado.java
@@ -4,27 +4,54 @@ public class Empleado {
     private String apellido;
     private float salarioBase;
 
+    public int getId() {
+        return id;
+    }
 
-    public Empleado(int id, String nombre, String apellido, float salarioBase) {
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public float getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(float salarioBase) {
         this.salarioBase = salarioBase;
     }
 
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "Empleado{" +
-                "id=" + id +
-                ", nombre=" + nombre +
-                ", apellido=" + apellido +
-                ", salarioBase=" + salarioBase +
-                '}';
+    public Empleado(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salarioBase = 1000;
+    }
+
+    public void mostrarInformacion(){
+        System.out.printf("Nombre %s\n", this.nombre);
+        System.out.printf("Apellido %s\n", this.apellido);
+        System.out.printf("Salario %f\n", this.salarioBase);
+        System.out.printf("id %d\n", this.id);
     }
 
     protected float calcularSalario()
     {
-
         return 0;
     }
 
